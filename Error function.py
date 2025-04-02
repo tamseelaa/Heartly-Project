42a035df13f0b662bbd239f04bc3089d591c78ff
tamseelaa <tamseelm@metropolia.fi>
2025-04-02 10:35:52 +0300

    History class and Error function

--- Error function.py
+++ Error function.py
@@ -1,3 +1,18 @@
+import time
+from machine import Pin, I2C, Timer
 def error(self):
-    print("Error Unable to send data /n Press button to retry /n or wait 30 sec")
+    i2c = I2C(1, scl=Pin(15), sda=Pin(14), freq=400000)
+    oled_width = 128
+    oled_height = 64
+    oled = SSD1306_I2C(oled_width, oled_height, i2c)
+    y=0
+    words="Error Unable to send data /n Press button to retry /n or wait 30 sec"
+    while True:
+    button.off()
+    oled.text(words, 0, y, 1)
+    oled.show()
+    else:
+        oled.fill(0)
+        oled.show()
+    time.sleep(0.05)
     return
--- History Class.py
+++ History Class.py
@@ -1,14 +1,31 @@
+import time
+from machine import Pin, I2C, Timer
 Class History():
     def __init__(self):
         self.latest_max_rows=max_rows[]
         self.measurement_no=measurement_no[]
         
-        
-    def measurements(self,latest_max_rows):
-        sql=f"select measurement from history order by desc() limit 8 "
-        return self.max_row[:8]
+    def measurements(self):
+        latest_max_row=f"select measurement from history order by desc() limit 8 "
+        return self.latest_max_row[:8]
         
     def measurement_data(self,measurement_no):
-        sql_measurement= f"select * from history where latest_max_row.measurement_no= {self.measurement_no} "
+        measurement_no= f"select * from history where latest_max_row.measurement_no= {self.measurement_no} "
         return self.measurement_no
-        
+
+i2c = I2C(1, scl=Pin(15), sda=Pin(14), freq=400000)
+oled_width = 128
+oled_height = 64
+oled = SSD1306_I2C(oled_width, oled_height, i2c)
+y=0
+measurements=History.measurements()
+while True:
+    button.off()
+    oled.text(measurements, 0, y, 1)
+    oled.show()
+    y=y+8
+    time.sleep(0.05)
+else:
+    oled.fill(0)
+    oled.show()
+    time.sleep(0.05)
